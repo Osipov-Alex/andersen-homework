8dfd826e5c7c08e0566c51ae000b9755f8f787e4
MuuShuu <MuuShuu@users.noreply.github.com>
2022-06-18 13:25:17 +0300

    fix requested changes

--- homework_1.1/index.js
+++ homework_1.1/index.js
@@ -1,11 +1,13 @@
 const convertNumber = () => {
-  const number = Number(prompt('Введите число', ''));
-  const base = Number(prompt('Введите число', ''));
+  const START_STRING = 'Введите число';
+  const number = Number(prompt(START_STRING, ''));
+  const base = Number(prompt(START_STRING, ''));
 
   if (isNaN(number) || isNaN(base)) {
+    console.error('Некорректный ввод!');
     return 'Некорректный ввод!';
   }
-
+  console.log(number.toString(base));
   return number.toString(base);
 }
 
--- homework_1.2/index.js
+++ homework_1.2/index.js
@@ -1,16 +1,26 @@
+const checkValue = (value) => {
+  if (isNaN(value)) {
+    console.error('Некорректный ввод!');
+    return false;
+  }
+  return true;
+}
+
 const sumAndQuotient = () => {
-  const number_1 = Number(prompt('Введите число', ''));
+  const startString = 'Введите число';
+  const number_1 = Number(prompt(startString, ''));
 
-  if (isNaN(number_1)) {
-    return 'Некорректный ввод!';
+  if (!checkValue(number_1)) {
+    return
   }
 
-  const number_2 = Number(prompt('Введите число', ''));
+  const number_2 = Number(prompt(startString, ''));
 
-  if (isNaN(number_2)) {
-    return 'Некорректный ввод!';
+  if (!checkValue(number_2)) {
+    return
   }
 
+  console.log(`Ответ: ${number_1 + number_2}, ${number_1 / number_2}.`);
   return `Ответ: ${number_1 + number_2}, ${number_1 / number_2}.`;
 }
 
